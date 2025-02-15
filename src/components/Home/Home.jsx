f9a33aac44a041036f14bdea3a19ab4dc79d1adb
AsmaaMahmoudMohamed <maha_mouhamed@yahoo.com>
2025-02-15 14:23:36 +0200

    second update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import style from "./Home.module.css";
 import RecentProducts from '../RecentProducts/RecentProducts';
 import CategoriesSlider from '../CategoriesSlider/CategoriesSlider';
-import MainSlider from '../mainSlider/mainSlider';
+import MainSlider from '../MainSlider/MainSlider';
 
 
 
